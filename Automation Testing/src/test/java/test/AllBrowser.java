c6d96afc3ac160b8e7dae180e84af4563b42f59a
aniksur034 <aniksur030@gmail.com>
2025-01-17 13:27:08 +0600

    Selenium Scripts for all browser

---
+++ Automation Testing/src/test/java/test/AllBrowser.java
@@ -0,0 +1,60 @@
+package test;
+//Default for all
+import org.openqa.selenium.WebDriver;
+
+//For Chrome Browser
+//import org.openqa.selenium.chrome.ChromeDriver;
+
+//For Firefox Browser
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+//For Edge Browser
+import org.openqa.selenium.edge.EdgeDriver;
+public class AllBrowser {
+    public static void main(String args[]) throws InterruptedException
+    {
+        // For Chrome Browser
+        /** 
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Apnik034\\eclipse-workspace\\OrangeHRM-Website-Testing\\Automation Testing\\Drivers\\chromedriver.exe");
+		 
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		 */
+
+        //For Firefox Browser
+        /**
+          System.setProperty("webdriver.gecko.driver", "C:\\Users\\Apnik034\\eclipse-workspace\\OrangeHRM-Website-Testing\\Automation Testing\\Drivers\\geckodriver.exe" );
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+         */
+
+        //For Edge Driver
+        /**
+        System.setProperty("webdriver.edge.driver", "C:\\Users\\Apnik034\\eclipse-workspace\\OrangeHRM-Website-Testing\\Automation Testing\\Drivers\\msedgedriver.exe");
+
+
+		WebDriver driver = new EdgeDriver();
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");  
+        */ 
+
+
+        //How to use [User.dir] path on the getProperty method)
+
+        System.setProperty("webdriver.edge.driver", System.getProperty("user.dir")+"\\Drivers\\msedgedriver.exe");
+
+
+		WebDriver driver = new EdgeDriver();
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        //full screen the window size
+        driver.manage().window().maximize();
+
+        //Close the Browser
+        Thread.sleep(5000);
+        driver.close();
+
+    }
+}
