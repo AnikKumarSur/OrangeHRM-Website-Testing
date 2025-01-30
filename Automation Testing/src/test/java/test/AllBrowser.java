c50894c2f73817cf82939503b7aaa3fb59ee6009
aniksur034 <aniksur030@gmail.com>
2025-01-30 23:05:49 +0600

    name locator and send value

--- Automation Testing/src/test/java/test/AllBrowser.java
+++ Automation Testing/src/test/java/test/AllBrowser.java
@@ -1,25 +1,26 @@
 package test;
+import org.openqa.selenium.By;
 //Default for all
 import org.openqa.selenium.WebDriver;
 
 //For Chrome Browser
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 //For Firefox Browser
 //import org.openqa.selenium.firefox.FirefoxDriver;
 
 //For Edge Browser
-import org.openqa.selenium.edge.EdgeDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
 public class AllBrowser {
     public static void main(String args[]) throws InterruptedException
     {
         // For Chrome Browser
-        /** 
+        
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Apnik034\\eclipse-workspace\\OrangeHRM-Website-Testing\\Automation Testing\\Drivers\\chromedriver.exe");
 		 
 		 WebDriver driver = new ChromeDriver();
 		 driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		 */
+		 
 
         //For Firefox Browser
         /**
@@ -42,19 +43,24 @@ public class AllBrowser {
 
         //How to use [User.dir] path on the getProperty method)
 
-        System.setProperty("webdriver.edge.driver", System.getProperty("user.dir")+"\\Drivers\\msedgedriver.exe");
+        //System.setProperty("webdriver.edge.driver", System.getProperty("user.dir")+"\\Drivers\\msedgedriver.exe");
 
 
-		WebDriver driver = new EdgeDriver();
+		//WebDriver driver = new EdgeDriver();
 
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		//driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
         //full screen the window size
-        driver.manage().window().maximize();
+       // driver.manage().window().maximize();
 
         //Close the Browser
-        Thread.sleep(5000);
-        driver.close();
+       
+        //How to use the name locator and send the value
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+                
+       
 
     }
 }
